d63631c6200385e61184a6ac95c19057d394a42d
Henry Lee - Dev <henrylee.contact@gmail.com>
2022-10-21 14:04:08 -0400

    files

---
+++ main.js
@@ -0,0 +1,47 @@
+const jsonfile = require('jsonfile')
+const moment = require('moment')
+const simpleGit = require('simple-git')
+
+const FILE_PATH = './data.json'
+
+const makeCommit = (x,y) => {
+  const DATE = moment().subtract(131, 'd')
+                        .add(x,'w').add(y,'d').format()
+  const data = {
+    date: DATE
+  }
+  jsonfile.writeFile(FILE_PATH, data, ()=>{
+    simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE}).push()
+  })
+}
+
+makeCommit(3,3)
+
+
+// RANDOMIZER
+
+
+const jsonfile = require('jsonfile')
+const moment = require('moment')
+const simpleGit = require('simple-git')
+const random = require('random')
+
+const FILE_PATH = './data.json'
+
+const makeCommit = n => {
+  if(n === 0) return simpleGit.push
+  const x = random.int(0,54)
+  const y = random.int(0,6)
+  const DATE = moment().subtract(131, 'd')
+                        .add(x,'w').add(y,'d').format()
+  const data = {
+    date: DATE
+  }
+  console.log(DATE)
+  jsonfile.writeFile(FILE_PATH, data, ()=>{
+    simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE}, 
+    makeCommit.bind(this, --n))
+  })
+}
+
+makeCommit(100)
